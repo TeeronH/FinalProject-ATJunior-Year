fb31f5599241ce6d358d9c8dbaf5c988a27db776
Teeron Hajebi <teeron@teerons-air.lan>
2021-05-13 21:35:40 -0700

    Kind of done I guess

--- Work.py
+++ Work.py
@@ -1,25 +1,36 @@
 
 import cv2
 import imutils
-from PIL import Image, ImageDraw, ImageFilter
-from graphics import *
-import sys
+from PIL import Image
 import numpy as np
+import os.path
+
+homedir = os.path.expanduser("~")
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-img = cv2.imread('FACE.jpg')
+img = cv2.imread(homedir + '/FinalProject-ATJunior-Year/FACE.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+	img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	roi_gray = gray[y:y+h, x:x+w]
+	roi_color = img[y:y+h, x:x+w]
+	first_image = Image.open('/Users/teeron/FinalProject-ATJunior-Year/FACE.jpg')
+	second_image = Image.open('/Users/teeron/FinalProject-ATJunior-Year/Shrek1.jpg')
+	second_image.thumbnail((w, h))
+	first_image.paste(second_image, (x,y))
+
    
-cv2.imshow('img',img)
+first_image.show()
+
+'''
+cv2.imshow('Git Shreked',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
+
 
 '''
 imagePath = sys.argv[1]
